0988ce54cac455829fdec3227823b4726254e4cf
Carlo Hamalainen <carlo@carlo-hamalainen.net>
2014-02-24 13:25:27 +0100

    Update clean script.

--- clean.sh
+++ clean.sh
@@ -1,2 +1,3 @@
 rm -fvr /scratch/fast-example/volgenmodel_output /scratch/fast-example/workflow /scratch/fast-example/fit-stage-??.conf
 rm -fvr /scratch/fast-example/work
+rm -fvr /scratch/fast-example/volgenmodel_final_output
