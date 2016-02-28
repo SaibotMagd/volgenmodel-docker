362988e59be85999e9dd0e0c65e03801705c725d
Carlo Hamalainen <carlo@carlo-hamalainen.net>
2016-02-28 20:33:25 +0800

    Use new model output.

--- docker/go.sh
+++ docker/go.sh
@@ -6,5 +6,5 @@ cd /opt
 
 python go.py
 
-mincdiff /scratch/volgenmodel-fast-example/model-2016-01-09.mnc \
+mincdiff /scratch/volgenmodel-fast-example/model-2016-02-28.mnc \
          /scratch/volgenmodel-fast-example/volgenmodel_final_output/model/mouse00_volcentre_norm_resample_bigaverage_reshape_vol_symm.mnc
