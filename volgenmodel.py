1fd8c3b49883e2d143f98be64c25e3c2900eb4fd
Isshaa Aarya <isshaa7@gmail.com>
2016-11-09 10:12:50 +1000

    Update volgenmodel.py

--- volgenmodel.py
+++ volgenmodel.py
@@ -246,7 +246,7 @@ def make_workflow():
     opt['pad'] = 5
     # opt['config_file'] = os.path.join(FAST_EXAMPLE_BASE_DIR, 'fit.10-genmodel.conf')
     opt['config_file'] = None
-    opt['fit_stages'] = 'lin, 2, 3'
+    opt['fit_stages'] = 'lin, 2'
     opt['output_model'] = 'model.mnc'
     opt['output_stdev'] = 'stdev.mnc'
     # opt['workdir'] = '/scratch/volgenmodel-fast-example/work'
