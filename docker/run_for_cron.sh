a117bf0fdf6124077c28670da137c7d7ceb07560
Isshaa Aarya <isshaa7@gmail.com>
2016-12-08 08:02:05 +1000

    Update run_for_cron.sh

--- docker/run_for_cron.sh
+++ docker/run_for_cron.sh
@@ -7,4 +7,4 @@ cd /home/carlo/production/volgenmodel-nipype-docker
 
 ./build.sh
 
-docker run -i -t iatest-i/carlo/volgenmodel-nipype /bin/bash /opt/go.sh
+docker run -i -t iatest-i/volgenmodel-nipype /bin/bash /opt/go.sh
